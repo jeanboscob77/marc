c14b8ed7f24df58b5e778b4765078e2feda1d5cb
Jean Bosco B <jeanboscob77@gmail.com>
2025-06-10 12:34:25 +0200

    customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: ["./components/**/*.{js,ts,jsx,tsx}", "./app/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "slide-up": {
+          "0%": { transform: "translateY(30px)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+      },
+      animation: {
+        "slide-up": "slide-up 0.8s ease-out forwards",
+      },
+    },
   },
   plugins: [],
 };
